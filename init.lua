9c8e753c6c1ec80bba7eb79ab0a368165124603c
Vaughan Kitchen <v.kitchen@gnoms.org>
2017-04-13 08:22:23 +1200

    try more than one font path before failing

--- init.lua
+++ init.lua
@@ -1,5 +1,16 @@
-e = loadfont("/usr/X11R6/lib/X11/fonts/TTF/DejaVuSans.ttf", 15)
+fonts = {
+    "/usr/X11R6/lib/X11/fonts/TTF/DejaVuSans.ttf",
+    "/usr/share/fonts/dejavu/DejaVuSans.ttf"
+    }
+
+for key, value in pairs(fonts) do
+    e = loadfont(value, 15)
+    if e == 0 then
+        break
+    end
+end
+
 if e ~= 0 then
-   error("Failed to load font!")
+    error("Failed to load a font!")
 end
 
