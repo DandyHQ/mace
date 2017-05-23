d9970e0f8c9f23687f76bde60d795c6cce7ca1eb
Vaughan Kitchen <v.kitchen@gnoms.org>
2017-05-23 14:19:07 +1200

    change lua location on travis

--- scripts/install_lua53.sh
+++ scripts/install_lua53.sh
@@ -3,5 +3,6 @@
 curl -R -O http://www.lua.org/ftp/lua-5.3.4.tar.gz
 tar zxf lua-5.3.4.tar.gz
 cd lua-5.3.4
+sed -i -e 's/INSTALL_TOP=.*$/INSTALL_TOP=\/usr/g' Makefile
 make linux test
 sudo make install
