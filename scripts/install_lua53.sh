afeeb29e3b5e910168ac1bd30f11e1927544d6c2
Vaughan Kitchen <v.kitchen@gnoms.org>
2017-05-23 14:49:06 +1200

    pkg config for lua

--- scripts/install_lua53.sh
+++ scripts/install_lua53.sh
@@ -6,3 +6,10 @@ cd lua-5.3.4
 sed -i -e 's/INSTALL_TOP=.*$/INSTALL_TOP=\/usr/g' Makefile
 make linux test
 sudo make install
+make pc | sudo dd of=/usr/lib/pkgconfig/lua53.pc
+sudo sh -c 'cat << EOF >> /usr/lib/pkgconfig/lua53.pc
+Name: lua53
+Version: 5.3.4
+Description: Lua runtime
+EOF
+'
