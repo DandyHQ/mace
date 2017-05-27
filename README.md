771f88a291f67f17a7c271fde9ecd65ec5c09857
Mytchel Hammond <mytchel@openmailbox.org>
2017-05-27 15:18:41 +1200

    Fix sequence test.

--- README.md
+++ README.md
@@ -23,15 +23,11 @@ Mace requires the following dependencies:
 # apt install meson libx11-dev libcairo2-dev lua5.3-dev
 ```
 
-Then go to [Compiling](#compiling)
-
 #### Fedora 25 
 
 ```
 # dnf install meson libX11-devel cairo-devel lua-devel
 ```
-
-Then go to [Compiling](#compiling)
 
 #### OpenBSD 6.1
 
@@ -39,11 +35,10 @@ Then go to [Compiling](#compiling)
 # pkg_add cairo lua%5.3
 ```
 
-Then go to [Compiling](#compiling)
-
 ### Compiling
 
-Once you have the dependencies install, Mace can be compiled as follows:
+Once you have the dependencies install, Mace can be compiled as 
+follows:
 
 ```
 $ mkdir build
@@ -61,7 +56,8 @@ $ mace
 
 The last command launches Mace.
 
-If you'd like to run the test suite to ensure Mace is functioning correctly. They can be run from ninja using:
+If you'd like to run the test suite to ensure Mace is functioning 
+correctly. They can be run from ninja using:
 
 ```
 $ ninja -C build test
--- tests/sequence.c
+++ tests/sequence.c
@@ -20,66 +20,84 @@ luaremove(lua_State *L, void *addr)
 int
 main(int argc, char *argv[])
 {
+  size_t len, s1len, s2len, s12len, s3len;
+  const char *s1 = "Hello there.";
+  const char *s2 = "This is a test.";
+  const char *s12 = "Hello there.This is a test.";
+  const char *s3 = "Hello test.";
   struct sequence *s;
-  uint8_t *s1 = (uint8_t *) "Hello there.";
-  uint8_t *s2 = (uint8_t *) "This is a test.";
-  uint8_t *s12 = (uint8_t *) "Hello there.This is a test.";
-  uint8_t *s3 = (uint8_t *) "Hello test.";
   uint8_t buf[512];
-  size_t len;
 
-  printf("Make empty sequence\n");
+  s1len = strlen(s1);
+  s2len = strlen(s2);
+  s12len = strlen(s12);
+  s3len = strlen(s3);
+  
+  printf("Start with empty sequence\n");
   s = sequencenew(NULL, 0);
   if (s == NULL) {
     return 1;
   }
 
-  if (!sequenceinsert(s, 0, s1, strlen((const char *) s1))) {
-    printf("failed to insert s1!\n");
+  printf("Insert %zu : '%s'\n", s1len, s1);
+
+  if (!sequenceinsert(s, 0, (const uint8_t *) s1, s1len)) {
+    printf("Failed to insert s1!\n");
     return 2;
   }
-  
-  if (!sequenceinsert(s, strlen((const char *) s1),
-		      s2, strlen((const char *) s2))) {
 
-    printf("failed to insert s2!\n");
+  len = sequenceget(s, 0, buf, sizeof(buf));
+  if (len != s1len) {
+    printf("Retrieved string has incorrect length: %zu expected %zu\n",
+	   len, s1len);
     return 3;
   }
 
-  len = sequenceget(s, 0, buf, sizeof(buf));
-  if (len != strlen((const char *) s12)) {
-    printf("length wrong after insert have %zu should have %zu\n",
-	   len, strlen((const char *) s12));
+  if (strcmp((const char *) buf, s1) != 0) {
+    printf("Retrieved string '%s' does not match expected '%s'!\n",
+	   buf, s1);
     return 4;
   }
-
-  if (strcmp((const char *) buf, (const char *) s12) != 0) {
-    printf("retrieved string '%s' and expected string '%s' do not match!\n",
-	   buf, s12);
+  
+  if (!sequenceinsert(s, s1len, (const uint8_t *) s2, s2len)) {
+    printf("Failed to insert s2!\n");
     return 5;
   }
 
-  if (!sequencedelete(s, 6, 16)) {
-    printf("sequence delete failed!\n");
+  len = sequenceget(s, 0, buf, sizeof(buf));
+  if (len != s12len) {
+    printf("Retrieved string has incorrect length: %zu expected %zu\n",
+	   len, s12len);
     return 6;
   }
 
-  len = sequenceget(s, 0, buf, sizeof(buf));
-
-  if (len != strlen((const char *) s3)) {
-    printf("length wrong after delete have %zu should have %zu\n",
-	   len, strlen((const char *) s3));
+  if (strcmp((const char *) buf, s12) != 0) {
+    printf("Retrieved string '%s' does not match expected '%s'!\n",
+	   buf, s12);
     return 7;
   }
 
-  if (strcmp((const char *) buf, (const char *) s3) != 0) {
-    printf("retrieved string '%s' and expected string '%s' do not match!\n",
-	   buf, s3);
+  if (!sequencedelete(s, 6, 16)) {
+    printf("Sequence delete failed!\n");
     return 8;
+  }
+
+  len = sequenceget(s, 0, buf, sizeof(buf));
+  if (len != s3len) {
+    printf("Length after delete have %zu should have %zu\n",
+	   len, s3len);
+    return 9;
+  }
+
+  if (strcmp((const char *) buf, (const char *) s3) != 0) {
+    printf("Retrieved string '%s' does not match expected '%s'!\n",
+	   buf, s3);
+    return 10;
   }
  
   printf("free sequence\n");
   sequencefree(s);
 
+  printf("Sequence tested\n");
   return 0;
 }
